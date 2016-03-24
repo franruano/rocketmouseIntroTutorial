6c721845d8fd53d90cc24780bd5d8c343c5eab24
Francisco Suarez <francisco.suarez@gmail.com>
2016-03-24 15:40:42 +0000

    Article part 1 completed

---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene("RocketMouse");
+	}
+}
