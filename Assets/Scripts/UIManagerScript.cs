aef748640ecb547879d189058d249db70c3153e2
Francisco Suarez <francisco.suarez@gmail.com>
2016-03-26 10:35:43 +0000

    Completed part two of article

--- Assets/Scripts/UIManagerScript.cs
+++ Assets/Scripts/UIManagerScript.cs
@@ -6,6 +6,7 @@ public class UIManagerScript : MonoBehaviour {
 
 	public Animator startButton;
 	public Animator settingsButton;
+	public Animator dialog;
 
 	public void StartGame() {
 		SceneManager.LoadScene("RocketMouse");
@@ -14,5 +15,13 @@ public class UIManagerScript : MonoBehaviour {
 	public void OpenSettings() {
 		startButton.SetBool("isHidden", true);
 		settingsButton.SetBool("isHidden", true);
+
+		dialog.SetBool("isHidden", false);
+	}
+		
+	public void CloseSettings() {
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+		dialog.SetBool("isHidden", true);
 	}
 }
