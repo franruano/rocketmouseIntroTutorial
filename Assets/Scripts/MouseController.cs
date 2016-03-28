a59ddc0c23a83b207a6bd4dae389e810cd69de01
Francisco Suarez <francisco.suarez@gmail.com>
2016-03-28 11:53:00 +0100

    Added score counter

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MouseController : MonoBehaviour {
 
@@ -30,6 +31,9 @@ public class MouseController : MonoBehaviour {
 	public AudioSource footstepsAudio;
 
 	public ParallaxScroll parallax;
+
+	public Text coinsLabel;
+
 
 	// Use this for initialization
 	void Start () {
@@ -100,36 +104,8 @@ public class MouseController : MonoBehaviour {
 		Destroy(coinCollider.gameObject);
 
 		AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
-	}
 
-	void OnGUI() {
-		DisplayCoinsCount();
-
-		DisplayRestartButton();
-	}
-
-	void DisplayCoinsCount() {
-		Rect coinIconRect = new Rect(10, 10, 32, 32);
-		GUI.DrawTexture(coinIconRect, coinIconTexture);                         
-		
-		GUIStyle style = new GUIStyle();
-		style.fontSize = 30;
-		style.fontStyle = FontStyle.Bold;
-		style.normal.textColor = Color.yellow;
-		
-		Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
-		GUI.Label(labelRect, coins.ToString(), style);
-	}
-
-	void DisplayRestartButton() {
-		if (dead && grounded)
-		{
-			Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
-			if (GUI.Button(buttonRect, "Tap to restart!"))
-			{
-				Application.LoadLevel (Application.loadedLevelName);
-			};
-		}
+		coinsLabel.text = coins.ToString();
 	}
 
 	void AdjustFootstepsAndJetpackSound(bool jetpackActive) {
